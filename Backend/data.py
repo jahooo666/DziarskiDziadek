22f4d6670d529a67fe2141ab26be84e0f77e7c65
Paweł <muchap1@ee.pw.edu.pl>
2016-03-12 00:51:09 +0100

    added functions for events sortng

---
+++ Backend/data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+categories_group = ["turystyka", "kultura", "aktywny wypoczynek", "warsztaty", "edukacja"]
+categories_indv = []
+
+events_lists = {'turystyka': [], 'kultura': [], 'aktywny_wypoczynek': [], 'warsztaty': [], 'edukacja': []}
+
+
+events = [
+    {'category': ['kultura', 'aktywny_wypoczynek'], 'nazwa': "Seans filmowy", 'adres': 'Waryńskiego 10', 'datetime': '15.10.2015 15:10'},
+    {'category': ['turystyka', "kultura"], 'nazwa': "Spacer filmowy", 'adres': 'Waryńskiego 10', 'datetime': '1.10.2015 15:10'},
+    {'category': ['aktywny_wypoczynek'], 'nazwa': u"Ćwiczenia filmowe", 'adres': 'Waryńskiego 10', 'datetime': '1.10.2015 15:10'},
+    {'category': ['edukacja'], 'nazwa': "Wykład filmowy", 'adres': 'Waryńskiego 10', 'datetime': '1.10.2015 15:10'}
+]
+
